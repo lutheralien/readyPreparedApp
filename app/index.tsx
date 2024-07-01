6239f8ad629bf47ed1e04a5f2a14c123cf6a655a
lutherALien-LazyLogic <luthereyandi@lazylogiclimited.com>
2024-07-01 09:29:39 +0000

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Page = () => {
+  return (
+    <View>
+      <Text>hi</Text>
+    </View>
+  );
+};
+
+export default Page;
